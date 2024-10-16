7dd0e3aee93a3f5ab0daf946c3b3598ddea4f4f1
권은혜 <a01027947353@gmail.com>
2024-10-16 12:14:59 +0900

    05 module, 06 iniput-ouput

---
+++ 05_module/og/team/teacher.py
@@ -0,0 +1,10 @@
+class Animal:
+    name = 'kamja'
+    
+    def greeting(self):
+        return 'hello'
+    
+    
+
+def gorilla():
+    return '크우아ㅡ카르ㅏ크랔러ㅏㅓ랑'
---
+++ 05_module/ohgiraffers.py
@@ -0,0 +1,4 @@
+def bear():
+    return "꾸어ㅏ엉"
+
+ohgiraffers = "오지라퍼스"
